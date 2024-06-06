e44fe7515263947d18b2e91790f72155dd97b2dc
Jeremy Hummel <jh@lambdal.com>
2024-06-06 14:57:05 -0400

    better filtering

--- dataset_browser/flask-backend/app.py
+++ dataset_browser/flask-backend/app.py
@@ -215,7 +215,7 @@ def get_videos(_id):
     for filter_key, sub_filters in filters.items():
         if isinstance(sub_filters, dict):
             min_val, max_val = sub_filters.get('0'), sub_filters.get('1')
-            if min_val is not None and max_val is not None:
+            if min_val is not None and min_val != -1 and max_val is not None and max_val != -1:
                 min_val = float(min_val)
                 max_val = float(max_val)
                 filtered_df = filtered_df[(filtered_df[filter_key] >= min_val) & (filtered_df[filter_key] <= max_val)]
